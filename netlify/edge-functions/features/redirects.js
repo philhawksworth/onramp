d743820c140e899169a5ef8149941e2ac03885a0
Phil Hawksworth <phil@netlify.com>
2022-10-18 12:15:19 +0100

    add redirects test

--- netlify/edge-functions/features/redirects.js
+++ netlify/edge-functions/features/redirects.js
@@ -1,13 +1,17 @@
-const HTMLConfirmation = `Redirects deployed!`;
+const HTMLConfirmation = `Redirects deployed! Now you can call your serverless API at <a href="/api/hello">/api/hello</a`;
 
 
-export default function hasFunctions() {
+export default async function hasFunctions() {
 
   console.log(`checking for redirects`);
   
-  if (true) {
+  const testURL = `${context.site.url}/api/hello`;
+  console.log(`Checking for a redirect to ${testURL}`);
+  
+  const call = await fetch(testURL);
+  if (call.status !== 404) {
     return HTMLConfirmation;
-  } 
+  }
 }
 
 
