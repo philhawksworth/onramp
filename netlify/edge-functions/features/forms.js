1127559eacd880ec7ff98e45d22a7f0c5fc43556
Phil Hawksworth <phil@netlify.com>
2022-10-20 13:09:53 +0100

    test with encoding of form name

--- netlify/edge-functions/features/forms.js
+++ netlify/edge-functions/features/forms.js
@@ -2,19 +2,24 @@ const HTMLConfirmation = `<p>You've added a form! Netlify will now accept submis
 
 export default async function hasForms(context) {
 
-  const testURL = `${context.site.url}/forms/`;
+  let testURL = `${context.site.url}/`;
+  testURL = testURL.replace('http://','https://');
   console.log(`Checking for a form which posts to ${testURL}`);
   
   // 2 second timeout:
-  const controller = new AbortController()
-  const timeoutId = setTimeout(() => controller.abort(), 2000)
+  // const controller = new AbortController()
+  // const timeoutId = setTimeout(() => controller.abort(), 2000)
   let call;
   try {
     call = await fetch(testURL, {
-      signal: controller.signal,
-      method: "POST"
+      // signal: controller.signal,
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({
+        "form-name": "contact"
+      })
     });
-    clearTimeout(timeoutId);
+    // clearTimeout(timeoutId);
   } catch {
     call = {
       status: 504
@@ -25,6 +30,3 @@ export default async function hasForms(context) {
     return HTMLConfirmation;
   }
 }
-
-
-
