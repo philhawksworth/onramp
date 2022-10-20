ab18380a21a576dc59545dda8fb0f16bd72fafb7
Phil Hawksworth <phil@netlify.com>
2022-10-20 13:37:26 +0100

    log cookie check

--- netlify/edge-functions/features/splittest.js
+++ netlify/edge-functions/features/splittest.js
@@ -3,7 +3,7 @@ const HTMLConfirmation = `<p>You enabled a split test!</p>`;
 
 export default async function hasFunctions(context) {
 
-  console.log(`Checking for a split test cookie`);
+  console.log(`Checking for a split test cookie, ${context.cookies.get("nf_ab")}`);
   if(context.cookies.get("nf_ab")){
     return HTMLConfirmation;
   };
