286bed3f318d88aefe6ea348fce7a74e7743e3f1
Phil Hawksworth <phil@netlify.com>
2022-10-20 16:51:02 +0100

    fun

---
+++ netlify/functions/hello.js
@@ -0,0 +1,10 @@
+exports.handler = async (event, context) => {
+  const {
+    name = "World"
+  } = event.queryStringParameters;
+
+  return {
+    statusCode: 200,
+    body: `Hello, ${name}`
+  };
+};
